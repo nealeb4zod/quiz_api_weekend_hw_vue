2ac6f37e5b229dcc0e44e9f902307aed8f992315
Neale Johnston <neale@team-johnston.com>
2020-11-28 07:06:27 +0000

    sic data returning from api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form>
+      <label for="categories">Categorie: </label>
+      <select
+        name="categories"
+        id="categories"
+        @change="onChange"
+        v-model="selectedCategory"
+      >
+        <option value="">All categories</option>
+        <option
+          v-for="category in categories"
+          :key="category.id"
+          :value="category.id"
+          >{{ category.name }}</option
+        >
+      </select>
+      <label for="difficulties">Difficulties: </label>
+      <select
+        name="difficulties"
+        id="difficulties"
+        @change="onChange"
+        v-model="selectedDifficulty"
+      >
+        <option value="">All difficulties</option>
+        <option
+          v-for="(difficulty, index) in difficulties"
+          :key="index"
+          :value="difficulty"
+          >{{ difficulty }}</option
+        >
+      </select>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  data() {
+    return {
+      selectedCategory: null,
+      selectedDifficulty: null,
+      questions: [],
+      categories: [],
+      difficulties: ["Easy", "Medium", "Hard"],
+    };
+  },
+  methods: {
+    getCategories() {
+      let url = "https://opentdb.com/api_category.php";
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          this.categories = data.trivia_categories;
+        });
+    },
+
+    getQuestions() {
+      let url =
+        "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple";
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+          this.questions = data.results;
+        });
+    },
+  },
+  mounted() {
+    this.getCategories();
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
